5585e7c4570d41db2206ced0e268cbc3b6f6b026
Rafał Chojnacki <113987919+RafalCho02@users.noreply.github.com>
2025-08-05 20:56:51 +0200

    Update urls.py

--- Onyx/urls.py
+++ Onyx/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include  # Dodano import include
+from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("website/", include("website.urls"))  # Opcjonalnie dodaj ukośnik na końcu
+    path('website/', include('website.urls')),
+    path('', lambda request: redirect('website/', permanent=False)),  # <--- DODANE
 ]
+
